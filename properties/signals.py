72a0b04af52195c6d6e744c69dc660eaad990db6
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-11-02 23:52:53 +0300

    Update signals.py

--- properties/signals.py
+++ properties/signals.py
@@ -2,12 +2,11 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.core.cache import cache
 from .models import Property
-from .utils import CACHE_KEY_ALL_PROPERTIES
 
 @receiver(post_save, sender=Property)
-def invalidate_cache_on_save(sender, **kwargs):
-    cache.delete(CACHE_KEY_ALL_PROPERTIES)
+def clear_cache_on_save(sender, **kwargs):
+    cache.delete('all_properties')
 
 @receiver(post_delete, sender=Property)
-def invalidate_cache_on_delete(sender, **kwargs):
-    cache.delete(CACHE_KEY_ALL_PROPERTIES)
+def clear_cache_on_delete(sender, **kwargs):
+    cache.delete('all_properties')
