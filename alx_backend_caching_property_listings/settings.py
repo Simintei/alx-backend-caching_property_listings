eeffcfe539588bda8eb528b3931b39e5384014f0
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-11-02 23:40:18 +0300

    Update settings.py

--- alx_backend_caching_property_listings/settings.py
+++ alx_backend_caching_property_listings/settings.py
@@ -107,6 +107,20 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+#Configure Redis cache backend
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
