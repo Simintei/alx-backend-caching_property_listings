56f244cf360a046b1a36c4fc6e5b5dcdcb5ad05f
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-11-02 23:43:29 +0300

    Update 0001_initial.py

--- properties/migrations/0001_initial.py
+++ properties/migrations/0001_initial.py
@@ -1,1 +1,5 @@
-Contains something
+('title', models.CharField(max_length=200)),
+('description', models.TextField()),
+('price', models.DecimalField(max_digits=10, decimal_places=2)),
+('location', models.CharField(max_length=100)),
+('created_at', models.DateTimeField(auto_now_add=True)),
